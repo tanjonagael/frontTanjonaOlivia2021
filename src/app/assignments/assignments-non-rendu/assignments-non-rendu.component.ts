354619459a4d6917408e754199101febefefb5e2
Olivia <oliviah1razaiharinosy@gmail.com>
2021-04-14 10:40:31 +0300

    frontend assignment non rendu

--- src/app/assignments/assignments-non-rendu/assignments-non-rendu.component.ts
+++ src/app/assignments/assignments-non-rendu/assignments-non-rendu.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AssignmentsService } from 'src/app/shared/assignments.service';
+import { AuthService } from 'src/app/shared/auth.service';
+import { Assignment } from '../../shared/assignment.model';
 
 @Component({
   selector: 'app-assignments-non-rendu',
@@ -7,9 +11,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentsNonRenduComponent implements OnInit {
 
-  constructor() { }
+  assignmentsRendu:Assignment[];
 
-  ngOnInit(): void {
-  }
+  page: number=1;
+  limit: number = 20;
+  totalDocs: number;
+  totalPages: number;
+  hasPrevPage: boolean;
+  prevPage: number;
+  hasNextPage: boolean;
+  nextPage: number;
+  count:number = 0;
+  tableSizes = [20, 25, 30, 40];
+  
+  
+  constructor(private assignmentsService:AssignmentsService,
+    private route:ActivatedRoute,
+    private router:Router, private auth : AuthService) {}
 
+    ngOnInit() {
+   
+      // on regarde s'il y a page= et limit = dans l'URL
+    this.route.queryParams.subscribe(queryParams => {
+    // console.log("Dans le subscribe des queryParams")
+        this.page = +queryParams.page || 1;
+        this.limit = +queryParams.limit || 20;
+    });
+    this.getAssignmentsNonRenduPagine();
+   
+    }
+
+    getAssignmentsNonRenduPagine() {
+      this.assignmentsService.getAssignmentsNonRenduPagine(this.page, this.limit)
+        .subscribe(data => {
+        this.assignmentsRendu = data.docs;
+        this.page = data.page;
+        this.limit = data.limit;
+        this.totalDocs = data.totalDocs;
+        this.count = data.totalDocs;
+        this.totalPages = data.totalPages;
+        this.hasPrevPage = data.hasPrevPage;
+        this.prevPage = data.prevPage;
+        this.hasNextPage = data.hasNextPage;
+        this.nextPage = data.nextPage;
+      });
+    }
+
+  tabSize(event){
+    
+    this.page = event;
+    console.log(this.page);
+    this.getAssignmentsNonRenduPagine();
+  }  
+
+  tableData(event): void {
+    this.limit = event.target.value;
+    this.page = 1;
+    this.getAssignmentsNonRenduPagine();
+  }  
 }
